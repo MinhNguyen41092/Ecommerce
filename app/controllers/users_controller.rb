906c4e5d869be4f3de63ed9c992584dfff6feaf7
Your Name <forever.alone92@gmail.com>
2017-05-15 09:48:13 +0700

    sign up
    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,89 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :show]
+  before_action :load_user, only: :show
+  before_action :require_logged_in, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :index
+
+  def index; end
 
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new user_params
     if @user.save
-      flash[:success] = "Your account has been created successfully"
+      log_in @user
+      flash[:success] = t "users.create"
       redirect_to root_path
     else
-      render 'new'
-    end
-  end
-
-  def edit; end
-
-  def update
-    if @user.update(user_params)
-      flash[:success] = "Your have updated your profile"
-      redirect_to user_path(@user)
-    else
-      render "edit"
+      render :new
     end
   end
 
   def show; end
 
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t "users.update"
+      redirect_to user_path @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find_by id: params[:id]
+    if @user.destroy
+      flash[:success] = t "users.deleted"
+      redirect_back
+    else
+      render :show
+    end
+  end
+
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
 
-    def set_user
-      @user = User.find(params[:id])
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t "users.no_user"
+    redirect_to root_path
+  end
+
+  def require_logged_in
+    unless logged_in?
+      flash[:danger] = t "users.pls_log_in"
+      redirect_to login_path
     end
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    if @user != current_user
+      flash[:danger] = t "users.not_correct_user"
+      redirect_to root_path
+    end
+  end
+
+  def admin_user
+    if !current_user.is_admin
+      flash[:danger] = t "users.not_admin"
+      redirect_to root_path
+    end
+  end
+
+  def redirect_back
+    if current_user.is_admin
+      redirect_to users_path
+    else
+      redirect_to root_path
+    end
+  end
 end
