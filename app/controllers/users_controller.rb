350b5dc285eb4199a3355ba6c12a27ae15fddfd6
Your Name <forever.alone92@gmail.com>
2017-05-15 09:48:13 +0700

    First commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Your account has been created successfully"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your have updated your profile"
+      redirect_to user_path(@user)
+    else
+      render "edit"
+    end
+  end
+
+  def show; end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+end
